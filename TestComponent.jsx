a5541d51d8b436959be8e9840af20a9bbaebcaac
Marwa Elgorn <marwa@example.com>
2025-10-21 04:29:21 +0300

    Add README and other changes

--- TestComponent.jsx
+++ TestComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function TestComponent() {
   return (
     <div>
-     <h3>This is my GitHub practice component</h3>; 
+     <h3>This is my GitHub practice component</h3>;  
     </div>
   )
 }
